f01ee0eca305bb790e7270662c36f31912603797
SAITO Tomomi <t-saito@project-flora.net>
2020-10-16 07:08:03 +0900

    Initial Commit

---
+++ DropdownMenu/DropdownMenu.h
@@ -0,0 +1,19 @@
+//
+//  DropdownMenu.h
+//  DropdownMenu
+//
+//  Created by SAITO Tomomi on 2020/10/16.
+//  Copyright © 2020 SAITO Tomomi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DropdownMenu.
+FOUNDATION_EXPORT double DropdownMenuVersionNumber;
+
+//! Project version string for DropdownMenu.
+FOUNDATION_EXPORT const unsigned char DropdownMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DropdownMenu/PublicHeader.h>
+
+
